6f52ac0350d09f8629d6b2a200ad9511a8c3bc44
damianlopez126 <731765@student.dusd.net>
2023-06-01 15:25:03 -0700

    final project
    added a infinite enemy spawner

--- MADITBFinalP1/Assets/Scripts/FollowPlayer.cs
+++ MADITBFinalP1/Assets/Scripts/FollowPlayer.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    private Transform target;
+    public GameObject player;
+    public float speed;
+    public float distanceBetween;
+
+    private float distance;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(target.position, transform.position) <= range)
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (distance < distanceBetween)
         {
-            FollowPlayer();
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
     }
-
-    public void FollowPlayer()
-    {
-        transform.position = Vector3.MoveToward(transform.position, target.transform.position, speed * Time.deltaTime);
-    }
 }
+
+    
